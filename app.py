a0f7a176773700ef527497ab2fe038362c9b870e
Johnny Quinn <jwq1228@g.rit.edu>
2023-03-28 03:09:51 -0400

    fun

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     api_key = "4744851151544ac1578cc038d586d99b"
-    city = "London"
+
+    # List of cities for the dropdown
+    cities = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'Philadelphia', 'San Antonio', 'San Diego', 'Dallas', 'San Jose', 'Austin', 'Jacksonville', 'Fort Worth', 'Columbus', 'San Francisco', 'Charlotte', 'Indianapolis', 'Seattle', 'Denver', 'Washington',
+              'Boston', 'Nashville', 'El Paso', 'Detroit', 'Memphis', 'Portland', 'Oklahoma City', 'Las Vegas', 'Louisville', 'Baltimore', 'Milwaukee', 'Albuquerque', 'Tucson', 'Fresno', 'Sacramento', 'Mesa', 'Atlanta', 'Kansas City', 'Colorado Springs', 'Miami', 'Raleigh',
+              'Omaha', 'Long Beach', 'Virginia Beach', 'Oakland', 'Minneapolis', 'Tulsa', 'Wichita', 'New Orleans', 'Arlington']
+
+
+    if request.method == 'POST':
+        # Get the selected city from the form
+        city = request.form['city']
+    else:
+        # Default city if no city selected yet
+        city = "London"
+
+    # Make the API request with the selected city
     url = f"http://api.weatherstack.com/current?access_key={api_key}&query={city}"
     response = requests.get(url)
     data = response.json()
@@ -35,7 +49,9 @@ def index():
                                           feels_like=feels_like,
                                           uv_index=uv_index,
                                           visibility=visibility,
-                                          is_day=is_day)
+                                          is_day=is_day,
+                                          cities=cities,
+                                          selected_city=city)
 
 if __name__ == '__main__':
     app.run()
